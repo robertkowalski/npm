105b4bc1eeadc2c0b3b38ff1aef3d44f527fac07
Robert Kowalski <rok@kowalski.gd>
2014-02-12 20:27:15 +0100

    fix failing test on linux

--- test/tap/lifecycle-signal.js
+++ test/tap/lifecycle-signal.js
@@ -1,19 +1,22 @@
 var test = require("tap").test
-var npm = require.resolve("../../bin/npm-cli.js")
+var npm = require("../../")
 var node = process.execPath
 var spawn = require("child_process").spawn
 var path = require("path")
 var pkg = path.resolve(__dirname, "lifecycle-signal")
 
+process.chdir(pkg)
 test("lifecycle signal abort", function (t) {
+  t.plan(1)
   // windows does not use lifecycle signals, abort
   if (process.platform === "win32") return t.end()
-  var child = spawn(node, [npm, "install"], {
-    cwd: pkg
+
+  npm.load({}, function (err) {
+    npm.install(".", function (er) {})
   })
-  child.on("close", function (code, signal) {
-    t.equal(code, null)
-    t.equal(signal, "SIGSEGV")
+
+  process.on("SIGSEGV", function (code, signal) {
+    t.pass("got signal")
     t.end()
   })
 })
